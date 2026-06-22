3365b57124f50c41fa325f1ac176d4bbabc17c14
Arjen Guis <guis.arjen@gmail.com>
2026-06-22 21:27:19 +0200

    remove psalmbord.regels functionality

--- transcrypt/python/pages/page_psalmbord.py
+++ transcrypt/python/pages/page_psalmbord.py
@@ -49,58 +49,11 @@ class Page(ElementWrapper):
         width_2 = 'col-sm-2'
         width_3 = 'col-sm-3'
 
-        def regel(text: str):
-            return {"text": text}
-
-        # col_1 = left column with config forms
-        ## weet niet of 75% | 25% verdeling op de pi goed verdeelt, aangezien die 25% wel minimaal 360px breed moet zijn
-        col_1 = E('div').attr('style', 'float: left; width: 75%')
-
-        # config item: titel regel
-        input_title = E('input').attr('class', 'form-control').attr('type', 'text')
-        col_1.append(
-            E('div').attr('class', 'form-group row').append(
-                E('label').attr('class', '{} col-form-label'.format(width_2)).inner_html("Titel"),
-                E('div').attr('class', '{}'.format(width_3)).append(input_title)
-            ),
-        )
-
-        div_list = E('div')
-        col_1.append(div_list)
-        plist = PagedList(div_list.element, "").hide_count().disable_pagination()
-        plist.get_styling().table_class('table borderless')
-
-        #config item: tekst regels
-        def text_element(attr, item):
-            r = E('input').attr('type', 'text').attr('style', 'width: 100%; font-family: monospace;')
-            r.element.value = item[attr]
-
-            def onchange(evt):
-                item[attr] = r.element.value
-                save_changes()
-            r.element.onchange = onchange
-            return r.element
-
-        plist.add_column('text', 'Regels').item_to_element(text_element.bind(None, 'text'))
-
         def set_inputs():
-            input_title.element.value = self.psalmbord['title']
-            plist.get_server().data = self.psalmbord['regels']
             select_fontfamily.element.value = self.psalmbord['fontfamily']
             select_fontweight.element.value = self.psalmbord['fontweight']
             select_refreshrate.element.value = self.psalmbord['refreshrate']
-            plist.refresh()
 
-            if len(self.psalmbord['screens']) == 0:
-                # self.psalmbord['screens'] is still empty at first time, fill with default screens. 
-                ## dit is misschien niet nodig als in settings.py bij upgrade() version 9:
-                ##   store['psalmbord']['screens'] = []
-                ## met dictionary van default screens gevuld wordt?
-                i = 0
-                for s in default_screens:
-                    add_screen('',i,s,default_fontsize,1)
-                    i = i + 1
-            
             i = 0
             for t in self.psalmbord['screens']:
                 ## idealiter zou ik de screen_index list opruimen, 
@@ -121,68 +74,19 @@ class Page(ElementWrapper):
                         save_changes()
                 i = i + 1
 
-        async def delete_regel(item):
-            self.psalmbord['regels'].remove(item)
-            await save_changes()
-
         async def save_changes():
             self.psalmbord = await utils.post(utils.get_url('general/setPsalmbord'), self.psalmbord)
             set_inputs()
-
-        plist.add_button('delete', '', 'btn btn-danger btn-sm') \
-            .use_element(lambda item: E('i').attr("class", 'fas fa-trash-alt')) \
-            .onclick(delete_regel)
-
-        async def change_order(up: bool, item):
-            regels = self.psalmbord['regels']
-            i = regels.index(item)
-            if not -1 < i < len(regels):
-                return
-            j = i - 1 if up else i + 1
-            j = max(0, min(j, len(regels) - 1))
-            regels.remove(item)
-            regels.insert(j, item)
-            self.psalmbord = await utils.post(utils.get_url('general/setPsalmbord'), self.psalmbord)
-            plist.get_server().data = self.psalmbord['regels']
-            plist.refresh()
-
-        plist.add_button('up', '', 'btn btn-primary btn-sm') \
-            .use_element(lambda item: E('i').attr("class", 'fas fa-sort-up').attr('style', 'font-size: 20px; vertical-align: bottom;')) \
-            .onclick(change_order.bind(None, True))
-
-        plist.add_button('down', '', 'btn btn-primary btn-sm') \
-            .use_element(lambda item: E('i').attr("class", 'fas fa-sort-down').attr('style', 'font-size: 20px; vertical-align: bottom;')) \
-            .onclick(change_order.bind(None, False))
-
-        # tekst regel toevoegen
-        def add_regel(evt):
-            self.psalmbord['regels'].append(regel(""))
-            plist.get_server().data = self.psalmbord['regels']
-            plist.refresh()
-
-        button_add_regel = E('button').attr('class', 'btn btn-primary btn-sm').inner_html("Regel toevoegen")
-        button_add_regel.element.onclick = add_regel
-
-        col_1.append(button_add_regel)
-
-        select_fontfamily = Select("fontfamily", fonts)
-        select_fontweight = Select("fontweight", fontweights)
-        select_refreshrate = Select("refreshrate", refreshrates)
-        
-        # custom screens
-        screen_index = []
-        screen_div = E('div').attr('class','row')
-
-        col_1.append( 
-            E('p').attr('class','psalmbord_heading').inner_html('Schermen'),
-            screen_div
-        )
 
         # add screen
         def add_screen(evt, i = None, text = '', fontsize = default_fontsize, active = None):
             if i is None:
                 i = screen_div.element.querySelectorAll(".screen").length
             screen_index.append( i )
+
+            select_fontsize = Select(f"fontsize{i}",fontsizes)
+            select_fontsize.element.value = fontsize
+            select_fontsize.element.onchange = onchange
 
             id = f'screen{i}'
             div = E('div').attr('class','{} screen'.format(width_2)).attr('data-id',i)
@@ -192,65 +96,51 @@ class Page(ElementWrapper):
                 s.element.checked = True
             s.element.onchange = onchange
             
-            select_fontsize = Select(f"fontsize{i}",fontsizes)
-            select_fontsize.element.value = fontsize
-            select_fontsize.element.onchange = onchange
+            f = E('div').attr('style','clear:both').append(
+                    E('label').attr('style', 'width:60%').inner_html("Aantal regels"),
+                    E('div').attr('style', 'display:inline-block;width:39%').append(select_fontsize)
+                )
+            
+            d = E('button').attr('class','btn btn-danger btn-sm').attr('style','float:right; margin: 5px 0;').append( E('i').attr("class", 'fas fa-trash-alt') )
+            d.element.onclick = delete_screen
 
-            if i == 0 and text == 'leeg':
-                l = 'Leeg'
-                f = None
-                d = None
-                t = None
-            elif i == 1 and text == 'regels':
-                l = 'Met regels'
-                f = E('div').append(
-                        E('label').attr('style', 'width:60%').inner_html("Aantal regels"),
-                        E('div').attr('style', 'display:inline-block;width:40%').append(select_fontsize)
-                    )
-                d = None
-                t = None
-            else:
-                l = 'Met tekst'
-                f = E('div').append(
-                        E('label').attr('style', 'width:60%').inner_html("Aantal regels"),
-                        E('div').attr('style', 'display:inline-block;width:40%').append(select_fontsize)
-                    )
-                
-                d = E('button').attr('class','btn btn-danger btn-sm').attr('style','float:right; margin: 5px 0;').append( E('i').attr("class", 'fas fa-trash-alt') )
-                d.element.onclick = delete_screen
+            t = E('textarea').attr('name',id)
+            t.element.value = text
+            t.element.onchange = onchange
 
-                t = E('textarea').attr('name',id)
-                t.element.value = text
-                t.element.onchange = onchange
-
-            div.append(
-                s,
-                E('label').attr('class','col-form-label').attr('for',id).inner_html( l ),
-            )
-            if d:
-                div.append(d)
-            if f:
-                div.append(f)
-            if t:
-                div.append(t)
+            div.append(s,d,f,t)
+            screen_div.append( div )
             
             self.psalmbord['screens'][i] = {'index':id,'text':text,'size':fontsize}
-            screen_div.append( div )
 
         def delete_screen(evt):
             evt.target.closest(".screen").remove()
             onchange()
 
+        # show output frame
+        screen_div = E('div').attr('class','row')
+        screen_div.append(
+            E('div').attr('style', 'float: left; width: 270px').append(
+                E('iframe').attr('src', '/psalmbord').attr('style', 'width: 270px; height: 480px;')
+            )
+        )
+
+        select_fontfamily = Select("fontfamily", fonts)
+        select_fontweight = Select("fontweight", fontweights)
+        select_refreshrate = Select("refreshrate", refreshrates)
+        screen_index = [] # nog nodig?
+
         button_add_screen = E('button').attr('class', 'btn btn-primary btn-sm').inner_html("Scherm toevoegen")
         button_add_screen.element.onclick = add_screen
 
-        col_1.append(button_add_screen)
+        self.append(screen_div)
+        self.append(button_add_screen)
 
         # spacer
-        col_1.append( E('p').attr('class','psalmbord_heading').inner_html('Instellingen') )
+        self.append( E('p').attr('class','psalmbord_heading').inner_html('Instellingen') )
 
         # config settings
-        col_1.append(
+        self.append(
             E('div').attr('class', 'form-group row').append(
                 E('label').attr('class', '{} col-form-label'.format(width_2)).inner_html("Lettertype"),
                 E('div').attr('class', '{}'.format(width_3)).append(select_fontfamily)
@@ -264,13 +154,6 @@ class Page(ElementWrapper):
                 E('div').attr('class', '{}'.format(width_3)).append(select_refreshrate)
             ),
         )
-        self.append(col_1)
-
-        # col_2 = right column with output frame
-        col_2 = E('div').attr('style', 'float: left; width: 25%').append(
-            E('iframe').attr('src', '/psalmbord').attr('style', 'width: 360px; height: 640px;')
-        )
-        self.append(col_2)
 
         async def initialize():
             self.psalmbord = await utils.post(utils.get_url('general/getPsalmbord'), {})
@@ -279,7 +162,6 @@ class Page(ElementWrapper):
         self.refresh = initialize
 
         async def onchange(evt):
-            self.psalmbord['title'] = input_title.element.value
             self.psalmbord['fontfamily'] = select_fontfamily.element.value
             self.psalmbord['fontsize'] = default_fontsize # default value, will be updated
             self.psalmbord['fontweight'] = select_fontweight.element.value
@@ -303,7 +185,6 @@ class Page(ElementWrapper):
                 i = i + 1
             save_changes()
 
-        input_title.element.onchange = onchange
         select_fontfamily.element.onchange = onchange
         select_fontweight.element.onchange = onchange
         select_refreshrate.element.onchange = onchange
