9e09c690b401d91888140519e166863a03141b9f
Arjen Guis <guis.arjen@gmail.com>
2026-07-09 22:14:50 +0200

    first steps camera page

--- transcrypt/python/pages/page_camera.py
+++ transcrypt/python/pages/page_camera.py
@@ -7,7 +7,55 @@ class Page(ElementWrapper):
 
     def __init__(self):
         super().__init__(element('div'))
-        self.attr('style', 'max-width: 1000px;')
+        self.cameras: dict = None
+        cam = 0
+
+        div_cams = E('div').attr('id','cams')
+        div_live = E('div').attr('id','live')
+        div_presets = E('div').attr('id','presets')
+        div_move = E('div').attr('id','move')
+        div_footer = E('div').attr('id','footer')
+
+        self.append(
+            div_cams,
+            div_live,
+            div_presets,
+            div_move,
+            div_footer
+        )
+
+        def btn_cameras():
+            ul = E('ul')
+            for index, cam in enumerate(self.cameras):
+                btn = E('button').inner_html(cam['name']).attr('value',index)
+                btn.element.onclick = btn_presets
+
+                ul.append(
+                    E('li').append( btn )
+                )
+            div_cams.append( ul )
+
+        def btn_presets(evt):
+            print(evt.target.value)
+            div_presets.remove_childs()
+            cam = evt.target.value
+            presets = self.cameras[cam]['presets']
+
+            if len(presets) == 0:
+                div_presets.append( E('p').inner_html("Geen presets") )
+            else:
+                for pr in presets:
+                    btn = E('button').attr('name','p').attr('value',pr['token']).inner_html(pr['token'])
+                    #btn.element.onclick = 
+
+                    div_presets.append( btn )
+        
+
+        async def initialize():
+            self.cameras = await utils.post(utils.get_url("general/getCameras"), {})
+            btn_cameras()
+
+        self.refresh = initialize
 
     def show(self):
         main.remove_childs()
